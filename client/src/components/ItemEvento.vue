c285f98de0c62dad5aacb4995b1a7e729725d8ed
FabianGuevaraPinilla <fabianguevara157@gmail.com>
2021-09-17 20:28:45 -0500

    Actualizacion de Eventos.vue

---
+++ client/src/components/ItemEvento.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="Eventos__item">
+
+    </div>
+</template>
+<script>
+export default {
+    props: ['dataEvento', 'simple'],
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
--- client/src/views/Eventos.vue
+++ client/src/views/Eventos.vue
@@ -1,3 +1,26 @@
 <template>
+  <div class="Eventos">
     <h1>Eventos</h1>
+    <div class="Eventos__lista" >
+        <ItemEvento :dataEvento = "item_evento" v-for = "item_evento in lista_eventos" :key="item_evento.id"></ItemEvento>
+    </div>
+  </div>
 </template>
+<script>
+import ItemEvento from "./../components/ItemEvento.vue";
+export default {
+  components: { ItemEvento: ItemEvento },
+  data() {
+    return {
+      lista_eventos: [], //inicializamos un array de datos vacío
+    };
+  },
+  mounted() {
+    fetch("https://my-json-server.typicode.com/DarkNikT/fakeapi-appweb/eventos")
+      .then((res) => res.json())
+      .then((data) => {
+        this.lista_eventos = data;
+      });
+  },
+};
+</script>
