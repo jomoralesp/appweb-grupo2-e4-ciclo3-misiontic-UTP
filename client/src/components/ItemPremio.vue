8730502a02033441e2d6baf283f51e74966e0a26
FabianGuevaraPinilla <fabianguevara157@gmail.com>
2021-09-21 22:40:52 -0500

    Modificación componente ItemPremios

--- client/src/components/ItemPremio.vue
+++ client/src/components/ItemPremio.vue
@@ -2,40 +2,31 @@
   <div class="ItemPremio card rounded-3" v-on:click="showAlert()">
     <div class="ItemPremio_img position-relative">
       <!-- Si el componente se encuentra en promo -->
-      <h1
-        class="
-          fw-bold
-          position-absolute
-          top-0
-          start-0
-          fs-4
-          w-25
-          py-2
-          ItemPremio_tituloPromo
-        "
+      <h1 
+      class="fw-bold position-absolute top-0 start-0 fs-4 w-25 py-2 ItemPremio_tituloPromo"
+      v-if="dataPremio.promo"
       >
         Promo
       </h1>
       <!-- IMagen del premio -->
       <img
-        class="img-fluid"
-        :src="require('@/assets/images/premios/Busocuelloredondoblanco.jpg')"
-        alt=""
+        class="img-fluid"  :src="require('@/assets'+ dataPremio.ulrImagen)" alt=""
       />
     </div>
     <div class="ItemPremio_puntos">
       <p class="fs-6 mb-0">PUNTOS</p>
       <!-- valor de puntos -->
-      <h1 class="fs-1">200</h1>
+      <h1 class="fs-1">{{dataPremio.puntos}}</h1>
     </div>
   </div>
 </template>
 <script>
 export default {
+  props: ['dataPremio'],
   methods: {
     showAlert() {
       // Use sweetalert2
-      this.$swal('Hello Vue world!!!');
+      this.$swal('Ha presionado el premio: ' + this.dataPremio.id);
     },
   },
 };
--- client/src/views/Premios.vue
+++ client/src/views/Premios.vue
@@ -2,18 +2,55 @@
   <div class="container">
     <h1>Premios</h1>
     <div class="Premios_listaPremios">
-      <ItemPremio></ItemPremio>
-      <ItemPremio></ItemPremio>
-      <ItemPremio></ItemPremio>
-      <ItemPremio></ItemPremio>
-      <ItemPremio></ItemPremio>
-      <ItemPremio></ItemPremio>
+      <ItemPremio v-for= "dataPremio in dataPremios" :key="dataPremio.id" :dataPremio="dataPremio" ></ItemPremio>
     </div>
   </div>
 </template>
 <script>
 import ItemPremio from "./../components/ItemPremio.vue";
 export default {
+  data(){
+    return{
+      dataPremios: [
+        {
+          id: 0,
+          promo: false,
+          puntos: 260,
+          ulrImagen: '/images/premios/sombrillarecogible.jpg'
+        },
+        {
+          id: 1,
+          promo: true,
+          puntos: 600,
+          ulrImagen: '/images/premios/Busocuelloredondoblanco.jpg'
+        },
+        {
+          id: 2,
+          promo: false,
+          puntos: 200,
+          ulrImagen: '/images/premios/camisetapoloblanco.jpg'
+        },
+        {
+          id: 3,
+          promo: true,
+          puntos: 400,
+          ulrImagen: '/images/premios/Busocuelloredondoblanco.jpg'
+        },
+        {
+          id: 4,
+          promo: false,
+          puntos: 400,
+          ulrImagen: '/images/premios/Busocuelloredondoblanco.jpg'
+        },
+        {
+          id: 5,
+          promo: true,
+          puntos: 260,
+          ulrImagen: '/images/premios/sombrillarecogible.jpg'
+        },
+      ]
+    }
+  },
   components: { ItemPremio },
 };
 </script>
